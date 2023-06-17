4a9e0dd46880ca18e0a06df347b5669b4261f78a
Andrey Khomich <supremum76@gmail.com>
2023-06-17 15:27:05 +0300

    2D CNN Model

--- ntm_cnn_ddpg/cnn/model2d.py
+++ ntm_cnn_ddpg/cnn/model2d.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from keras.models import Sequential as TensorFlowSequentialModel
 from keras.layers import Layer, Input, Dense, Conv2D, Flatten, MaxPooling2D, BatchNormalization
+from tensorflow import TensorShape
 
 Tensor = tf.Tensor
 
@@ -54,8 +55,8 @@ class Model2D:
         F[i] = F[i-1] * ( 1 + rate_of_filters_increase)
         :param rate_of_kernel_size_increase: Коэффициент увеличения ядра свертки в каждом новом модуле модели по формуле
         K[i] = K[i-1] * ( 1 + rate_of_kernel_size_increase)
-        :param rate_of_pool_size_increase: Коэффициент увеличения окна объединения в каждом новом модуле модели по формуле
-        P[i] = P[i-1] * ( 1 + rate_of_pool_size_increase)
+        :param rate_of_pool_size_increase: Коэффициент увеличения окна объединения в каждом новом модуле модели
+        по формуле P[i] = P[i-1] * ( 1 + rate_of_pool_size_increase)
         """
         self.input_2d_shape = input_2d_shape
         self.input_1d_shape = input_1d_shape
@@ -69,25 +70,47 @@ class Model2D:
         self.__model = self.__model_build()
 
     def __model_build(self) -> TensorFlowSequentialModel:
-        model = TensorFlowSequentialModel()
+        model: TensorFlowSequentialModel = TensorFlowSequentialModel()
 
         input_shape: tuple[int, int, int] = (self.input_2d_shape[0], self.input_2d_shape[1],
                                              self.input_2d_shape[2] + self.input_1d_shape[0] * self.input_1d_shape[1])
         layer: Layer = Input(shape=input_shape, dtype=tf.float32)
         model.add(layer=layer)
-        output_shape = layer.compute_output_shape(input_shape=input_shape)
+        output_shape: TensorShape = layer.compute_output_shape(input_shape=input_shape)
+
+        module_filters: float = self.start_filters
+        module_kernel_size: list[float, float] = list(self.start_kernel_size)
+        module_pool_size: list[float, float] = list(self.start_pool_size)
 
         # выполняем цикл пока не получим на выходе сверточной сети тензор с формой (1, 1, filters).
         # Пока не сведем размер 2D плоскости к точке с некоторым количеством фильтров.
         # Применять свертку и объединение к такой точке уже не требуется.
         while output_shape[0] == 1 and output_shape[1] == 1:
-            input_shape = output_shape
             # строим модуль, состоящий из последовательности слоев
+            module_input_shape = output_shape  # форма входа совпадает с выходом последнего слоя предыдущего модуля
+
             # слой нормализации
+            model.add(layer=BatchNormalization())
+
             # слой свертки
+            conv: Conv2D = Conv2D(kernel_size=(int(module_kernel_size[0]), int(module_kernel_size[1])),
+                                  input_shape=module_input_shape,
+                                  filters=int(module_filters),
+                                  padding="same",
+                                  activation=tf.nn.softsign)
+            model.add(layer=conv)
+            output_shape = conv.compute_output_shape(input_shape=module_input_shape)
+
             # слой объединения
-            output_shape = layer.compute_output_shape(input_shape=input_shape)
-            break
+            max_pooling: MaxPooling2D = MaxPooling2D(pool_size=(int(module_pool_size[0]), int(module_pool_size[1])))
+            model.add(layer=max_pooling)
+            output_shape = max_pooling.compute_output_shape(input_shape=output_shape)
+
+            module_filters *= (1 + self.rate_of_filters_increase)
+            module_kernel_size[0] *= (1 + self.rate_of_kernel_size_increase)
+            module_kernel_size[1] *= (1 + self.rate_of_kernel_size_increase)
+            module_pool_size[0] *= (1 + self.rate_of_pool_size_increase)
+            module_pool_size[1] *= (1 + self.rate_of_pool_size_increase)
 
         # выход модели окончательно вычисляет полносвязный слой персептрона с линейной функцией активации нейронов
         model.add(layer=Flatten())
@@ -132,3 +155,31 @@ class Model2D:
                                dtype=tf.dtypes.float32),
             shape=(batch_size, self.input_2d_shape[0], self.input_2d_shape[1], number_of_filters * length)
         )
+
+    def predict(self, input_2d: Tensor, input_1d: Tensor) -> Tensor:
+        """
+        Фычисление функции модели. Обучение модели при этом не выполняется.
+        :param input_2d: 2D вход модели в виде тензора формы (HEIGHT, WEIGHT, FILTERS FOR INPUT) или,
+        в случае вычисления для списка входных данных, (BATCH SIZE, HEIGHT, WEIGHT, FILTERS FOR INPUT).
+        Параметры HEIGHT, WEIGHT и FILTERS FOR INPUT должны соответствовать структуре модели.
+        :param input_1d: 1D вход модели в виде тензора формы (LENGTH, FILTERS FOR INPUT) или,
+        в случае вычисления для списка входных данных, (BATCH SIZE, LENGTH, FILTERS FOR INPUT).
+        Параметры LENGTH и FILTERS FOR INPUT должны соответствовать структуре модели.
+        :return: 1D тензор результатов вычисления функции модели в форме (LENGTH, FILTERS FOR OUTPUT) или,
+        в случае вычисления для списка входных данных, (BATCH SIZE, LENGTH, FILTERS FOR OUTPUT)
+        """
+        _input_2d: Tensor
+        _input_1d: Tensor
+        batch_mode: bool = len(input_2d.shape) == 4
+        if not batch_mode:
+            # преобразем данные к формату пакеты из одного примера входных даных.
+            # Это позволит выполнять дальнейшие вычисления единообразно.
+            _input_2d = tf.reshape(tensor=input_2d, shape=(1, input_2d.shape[0], input_2d.shape[1], input_2d.shape[2]))
+            _input_1d = tf.reshape(tensor=input_1d, shape=(1, input_1d.shape[0], input_1d.shape[1]))
+        else:
+            _input_2d = input_2d
+            _input_1d = input_1d
+
+        #  преобразуем 1d тензор к 2d тензору и конкатенируем все полученные тензоры по оси фильтра
+        return self.__model(inputs=tf.concat(values=(_input_2d, self.__1d_to_2d_input_tensor(_input_1d)), axis=3))
+
